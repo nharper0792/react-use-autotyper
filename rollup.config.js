39c813dd472288675d757a09286596a97be1999b
Nate Harper <78376389+nharper0792@users.noreply.github.com>
2024-01-30 00:29:25 -0500

    trying to figure this out

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import sass from 'rollup-plugin-sass';
+import {terser} from "rollup-plugin-terser";
 
 const devMode = (process.env.NODE_ENV === 'development');
 console.log(`${ devMode ? 'development' : 'production' } mode bundle`);
@@ -10,24 +11,24 @@ export default [
         input: 'src/index.ts', // Changed extension to .tsx
         output: {
             file: 'dist/index.js',
-            format: 'cjs',
+            format: 'es',
             sourcemap: devMode ? 'inline' : false,
         },
         plugins: [
             typescript(), // TypeScript plugin added first
             sass({ insert: true }),
-            // terser({
-            //     ecma: 2020,
-            //     mangle: { toplevel: true },
-            //     compress: {
-            //         module: true,
-            //         toplevel: true,
-            //         unsafe_arrows: true,
-            //         drop_console: !devMode,
-            //         drop_debugger: !devMode
-            //     },
-            //     output: { quote_style: 1 }
-            // })
+            terser({
+                ecma: 2020,
+                mangle: { toplevel: true },
+                compress: {
+                    module: true,
+                    toplevel: true,
+                    unsafe_arrows: true,
+                    drop_console: !devMode,
+                    drop_debugger: !devMode
+                },
+                output: { quote_style: 1 }
+            })
         ]
 
     }
